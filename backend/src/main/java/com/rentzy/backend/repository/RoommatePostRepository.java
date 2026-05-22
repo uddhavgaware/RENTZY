6f1e4696be1a3501502ec7d2836d57d3cd9ae7ca
uddhavgaware45 <udaygaware8@gmail.com>
2026-05-22 07:14:47 -0700

    Fix LazyInitializationException in Roommate API

--- backend/src/main/java/com/rentzy/backend/repository/RoommatePostRepository.java
+++ backend/src/main/java/com/rentzy/backend/repository/RoommatePostRepository.java
@@ -1,9 +1,19 @@
 package com.rentzy.backend.repository;
 
 import com.rentzy.backend.domain.RoommatePost;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.lang.Nullable;
 
 public interface RoommatePostRepository extends JpaRepository<RoommatePost, Long>, JpaSpecificationExecutor<RoommatePost> {
+
+    @EntityGraph(attributePaths = {"user", "images"})
+    Page<RoommatePost> findAll(@Nullable Specification<RoommatePost> spec, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"user", "images"})
+    Page<RoommatePost> findAll(Pageable pageable);
 }
